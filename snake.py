6b2903e6e4e887e653fe83a13feb70624cf34fc0
Metarus <rana.lulla@gmail.com>
2020-05-22 22:37:30 +0530

    Commit 2
    Reviving this very dead repo, might do some more work on it at some point but probably not. I wanted to revisit machine learning and so I decided to relearn the skills that I'd used for this, but make it slightly better. My main gripe with my former design was that it could loop around, so the snake would very quickly learn to just abuse that; find a simple pattern that has no overlapping that primarily goes down and to the right. I wanted to do something slightly better, so I redid it. Now, walls kill the snake, and that means that the snake has to properly learn. To make this work, I had to use a lot less inputs (I previously had 300), so I settled on 8; the tiles directly adjacent to the head, and the 4 directions in which the food could be, up, right, down, left. By keeping all inputs as binary and having a small number, it learns very quickly and with maybe half an hour or forty minutes of training, it can consistently hit around 15-20 points per game, which I'd say is pretty good.

---
+++ snake.py
@@ -0,0 +1,240 @@
+import pygame
+import random
+import network
+import numpy as np
+
+pygame.init()
+
+size=[400, 400]
+screen=pygame.display.set_mode(size)
+
+pygame.display.set_caption("Machine Learning Snake")
+
+done=False
+clock=pygame.time.Clock()
+
+sampleSize=50
+sampleCycle=0
+
+timer=100
+
+score=0
+highScore=0
+
+genNum=0
+variantNum=0
+
+goalLocations=[]
+for i in range(0, sampleSize):
+    goalLocations.append([])
+    for j in range(0, 400):
+        goalLocations[i].append([random.randint(0, 9), random.randint(0, 9)])
+
+tiles=[]
+for i in range(0, 10):
+    tiles.append([])
+    for j in range(0, 10):
+        tiles[i].append(0)
+
+def scoreadd():
+    global score
+    score+=1/sampleSize
+
+def settimer(val):
+    global timer
+    timer=val
+
+def addtimer(val):
+    global timer
+    timer+=val
+
+class Snake:
+    def __init__(self):
+        self.currentGoalLoc=[]
+        self.snekTiles=[]
+        self.skip=0
+        for i in range(0, 3):
+            self.snekTiles.append([5, 5])
+
+    def updateTiles(self):
+        for i in self.snekTiles:
+            tiles[i[0]][i[1]]=1
+        tiles[self.snekTiles[0][0]][self.snekTiles[0][1]]=2
+
+    def clearSnake(self):
+        tiles[1][1] = 1
+        for i in self.snekTiles:
+            tiles[i[0]][i[1]]=0
+
+    def headX(self): return self.snekTiles[0][0]
+    def headY(self): return self.snekTiles[0][1]
+
+    def newGoal(self):
+        notDone=True
+        while notDone:
+            p=goalLocations[sampleCycle][len(self.snekTiles)-3+self.skip]
+            notDone=False
+            for i in self.snekTiles:
+                if (i==p):
+                    notDone=True
+                    self.skip+=1
+            if not notDone:
+                tiles[p[0]][p[1]]=3
+                self.currentGoalLoc=[p[0], p[1]]
+
+    def move(self, dir):
+        temp=self.snekTiles[len(self.snekTiles)-1]
+        for i in self.snekTiles:
+            tiles[i[0]][i[1]]=0
+        for i in range(len(self.snekTiles)-1, 0, -1):
+            self.snekTiles[i]=self.snekTiles[i-1]
+
+        if(dir==0):
+            if(self.snekTiles[0][1]==0):
+                settimer(0)
+            else: self.snekTiles[0]=[self.snekTiles[0][0],(self.snekTiles[0][1]-1)]
+        if(dir==1):
+            if (self.snekTiles[0][0]==9):
+                settimer(0)
+            else: self.snekTiles[0]=[(self.snekTiles[0][0]+1),self.snekTiles[0][1]]
+        if(dir==2):
+            if (self.snekTiles[0][1]==9):
+                settimer(0)
+            else: self.snekTiles[0]=[self.snekTiles[0][0],(self.snekTiles[0][1]+1)]
+        if(dir==3):
+            if (self.snekTiles[0][0]==0):
+                settimer(0)
+            else:
+                self.snekTiles[0]=[(self.snekTiles[0][0]-1),self.snekTiles[0][1]]
+
+        if (tiles[self.snekTiles[0][0]][self.snekTiles[0][1]]==3):
+            self.snekTiles.append(temp)
+            self.newGoal()
+            scoreadd()
+            addtimer(50)
+
+        for i in self.snekTiles[1:]:
+            if(self.snekTiles[0]==i):
+                settimer(0)
+
+Snek=Snake()
+Snek.newGoal()
+
+net=network.NeuralNetwork([8, 4])
+
+clock = pygame.time.Clock()
+
+while not done:
+    clock.tick(30)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_UP:
+            Snek.move(0)
+        if event.key == pygame.K_RIGHT:
+            Snek.move(1)
+        if event.key == pygame.K_DOWN:
+            Snek.move(2)
+        if event.key == pygame.K_LEFT:
+            Snek.move(3)
+
+    netInput=[]
+    """for i in range(0, len(tiles)):
+        for j in range(0, len(tiles[i])):
+            temp = tiles[i][j]
+            for k in range(0, 4):
+                if temp==k:
+                    netInput.append(1)
+                else: netInput.append(0)"""
+    if Snek.headY()!=0:
+        if tiles[Snek.headX()][Snek.headY() - 1]==0:
+            netInput.append(1)
+        else: netInput.append(0)
+    else: netInput.append(0)
+    if Snek.headX()!=9:
+        if tiles[Snek.headX() + 1][Snek.headY()]==0:
+            netInput.append(1)
+        else: netInput.append(0)
+    else: netInput.append(0)
+    if Snek.headY()!=9:
+        if tiles[Snek.headX()][Snek.headY() + 1]==0:
+            netInput.append(1)
+        else: netInput.append(0)
+    else: netInput.append(1)
+    if Snek.headX()!=0:
+        if tiles[Snek.headX() - 1][Snek.headY()]==0:
+            netInput.append(1)
+        else: netInput.append(0)
+    else: netInput.append(0)
+    if Snek.currentGoalLoc[1]<Snek.headY():
+        netInput.append(1)
+    else: netInput.append(0)
+    if Snek.currentGoalLoc[0]>Snek.headX():
+        netInput.append(1)
+    else: netInput.append(0)
+    if Snek.currentGoalLoc[1]>Snek.headY():
+        netInput.append(1)
+    else: netInput.append(0)
+    if Snek.currentGoalLoc[0]<Snek.headX():
+        netInput.append(1)
+    else: netInput.append(0)
+
+    inputs=net.func(netInput)
+
+    if inputs[0]>inputs[1] and inputs[0]>inputs[2] and inputs[0]>inputs[3]:
+        Snek.move(0)
+    elif inputs[1]>inputs[2] and inputs[1]>inputs[3]:
+        Snek.move(1)
+    elif inputs[2]>inputs[3]:
+        Snek.move(2)
+    else: Snek.move(3)
+
+    Snek.updateTiles()
+
+    timer-=1
+
+    if timer<=0:
+        sampleCycle+=1
+        if sampleCycle>=sampleSize:
+            if score > highScore:
+                highScore = score
+                net.bestVals()
+                genNum += 1
+                variantNum = 0
+                text = open("wb.txt", 'w')
+                text.write(str(net.wb))
+                text.close()
+                print("New Generation")
+            print("Generation:", genNum, "Variant:", variantNum, "High:", round(highScore*10)/10, "Last:", round(score*10)/10)
+            net.nudge(10*np.power(1.1, variantNum/10000))
+            variantNum+=1
+            score=0
+            sampleCycle=0
+        Snek.clearSnake()
+        Snek.__init__()
+        timer=100
+        for i in range(0, 10):
+            for j in range(0, 10):
+                tiles[i][j]= 0
+        Snek.newGoal()
+        Snek.skip=0
+
+    for i in range(0, 10):
+        for j in range(0, 10):
+            color=(255, 0, 255)
+            if tiles[i][j]==0:
+                color=(0, 0, 0)
+            if tiles[i][j]==1:
+                color=(255, 255, 255)
+            if tiles[i][j]==2:
+                color=(255, 0, 255)
+            if tiles[i][j]==3:
+                color=(random.random()*255, random.random()*255, random.random()*255)
+            pygame.draw.rect(screen, color, [i*40, j*40, 40, 40])
+
+    pygame.display.flip()
+
+pygame.quit()
